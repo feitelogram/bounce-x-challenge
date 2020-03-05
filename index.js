0f0c99071df8c2bc6ca4dde5ca0b7cc85fc37fd5
Nicholas Feitel <Nicholas.Feitel@gmail.com>
2020-03-05 15:09:29 -0500

    done

--- index.js
+++
@@ -1,8 +0,0 @@
-const products = document.querySelectorAll(".mini-cart-product")
-const numItems = products.length
-const total = document.querySelector(".order-value").innerText
-
-products.forEach(product => {
-    let imgUrl = product.querySelector(".mini-cart-image").querySelector("a").querySelector("img").src
-    let name = product.querySelector(".mini-cart-name").querySelector("a").title.split(": ")[1]
-})
